43680b0405ac9ae5d880267e2e95fcb39a0f60dc
ftnext <takuyafjp+develop@gmail.com>
2023-06-24 00:09:30 +0900

    [test] Add test of to_character_date

---
+++ tests/test_core.py
@@ -0,0 +1,10 @@
+from datetime import date
+
+import the_solitary_castle_in_the_mirror._date as d
+import the_solitary_castle_in_the_mirror.core as sut
+
+
+def test_キャラクターのパラレルワールドに変換できる():
+    actual = sut.to_character_date(date(2022, 12, 23), "kokoro")
+    expected = d.DateInKokoroWorld(date(2005, 12, 23))
+    assert actual == expected
