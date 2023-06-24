3984434cbe065168321b1b146491b099749d5902
ftnext <takuyafjp+develop@gmail.com>
2023-06-24 17:32:11 +0900

    [refactor] Convert to test class to add other case

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,29 +3,32 @@ from freezegun import freeze_time
 from the_solitary_castle_in_the_mirror.__main__ import _main
 
 
-@freeze_time("2022-12-23")
-def test_それぞれのパラレルワールドの曜日を返せる():
-    the_seven = [
-        "kokoro",
-        "rion",
-        "aki",
-        "fuka",
-        "subaru",
-        "masamune",
-        "ureshino",
-    ]
-    expected = {
-        "kokoro": "Friday",
-        "rion": "Friday",
-        "aki": "Monday",
-        "fuka": "Monday",
-        "subaru": "Sunday",
-        "masamune": "Sunday",
-        "ureshino": "Wednesday",
-    }
+class Test_それぞれのパラレルワールドの曜日を返せる:
+    @freeze_time("2022-12-23")
+    def test_5月から12月(self):
+        the_seven = [
+            "kokoro",
+            "rion",
+            "aki",
+            "fuka",
+            "subaru",
+            "masamune",
+            "ureshino",
+        ]
+        expected = {
+            "kokoro": "Friday",
+            "rion": "Friday",
+            "aki": "Monday",
+            "fuka": "Monday",
+            "subaru": "Sunday",
+            "masamune": "Sunday",
+            "ureshino": "Wednesday",
+        }
 
-    actual = _main(the_seven)
-    assert actual == expected
-    assert (
-        the_seven == [name for name in actual] == [name for name in expected]
-    )
+        actual = _main(the_seven)
+        assert actual == expected
+        assert (
+            the_seven
+            == [name for name in actual]
+            == [name for name in expected]
+        )
