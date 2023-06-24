5a1ea7794cd7643f0af3180e0e2d13f50ee1e7ad
ftnext <takuyafjp+develop@gmail.com>
2023-06-24 16:45:52 +0900

    [feat] Calculate weekday in parallel world

--- tests/test_core.py
+++ tests/test_core.py
@@ -4,6 +4,10 @@ import the_solitary_castle_in_the_mirror._date as d
 import the_solitary_castle_in_the_mirror.core as sut
 
 
+def test_パラレルワールドの曜日を返せる():
+    assert sut.calculate_weekday("kokoro", date(2022, 12, 23)) == "Friday"
+
+
 def test_キャラクターのパラレルワールドに変換できる():
     actual = sut.to_character_date(date(2022, 12, 23), "kokoro")
     expected = d.DateInKokoroWorld(date(2005, 12, 23))
--- the_solitary_castle_in_the_mirror/core.py
+++ the_solitary_castle_in_the_mirror/core.py
@@ -7,5 +7,10 @@ from the_solitary_castle_in_the_mirror._date import _mappings as mappings
 from the_solitary_castle_in_the_mirror._types import CharacterName
 
 
+def calculate_weekday(character: str, date_: date) -> str:
+    character_date = to_character_date(date_, character)
+    return character_date.weekday()
+
+
 def to_character_date(date_: date, character: str) -> CharacterDate:
     return mappings[CharacterName.from_str(character)].create(date_)
