eb66729ac03413519a3571190d05982655395ae3
ftnext <takuyafjp+develop@gmail.com>
2023-06-24 18:57:08 +0900

    [chore] Package version as dynamic

--- the_solitary_castle_in_the_mirror/__init__.py
+++ the_solitary_castle_in_the_mirror/__init__.py
@@ -9,6 +9,8 @@ from the_solitary_castle_in_the_mirror.core import (  # NOQA: F401
     calculate_weekday,
 )
 
+__version__ = "0.1.0"
+
 characters: list[Character] = cast(
     list[Character],
     [str(value) for value in CharacterNameEnum.__members__.values()],
