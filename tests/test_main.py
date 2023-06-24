b5610ce8d02c6601ecb744e55d19e5c3f491fb99
ftnext <takuyafjp+develop@gmail.com>
2023-06-24 17:38:15 +0900

    [test] Support from Jan to Apr

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,17 +4,18 @@ from the_solitary_castle_in_the_mirror.__main__ import _main
 
 
 class Test_それぞれのパラレルワールドの曜日を返せる:
+    the_seven = [
+        "kokoro",
+        "rion",
+        "aki",
+        "fuka",
+        "subaru",
+        "masamune",
+        "ureshino",
+    ]
+
     @freeze_time("2022-12-23")
     def test_5月から12月(self):
-        the_seven = [
-            "kokoro",
-            "rion",
-            "aki",
-            "fuka",
-            "subaru",
-            "masamune",
-            "ureshino",
-        ]
         expected = {
             "kokoro": "Friday",
             "rion": "Friday",
@@ -25,10 +26,30 @@ class Test_それぞれのパラレルワールドの曜日を返せる:
             "ureshino": "Wednesday",
         }
 
-        actual = _main(the_seven)
+        actual = _main(self.the_seven)
         assert actual == expected
         assert (
-            the_seven
+            self.the_seven
+            == [name for name in actual]
+            == [name for name in expected]
+        )
+
+    @freeze_time("2023-01-10")
+    def test_1月から4月(self):
+        expected = {
+            "kokoro": "Tuesday",
+            "rion": "Tuesday",
+            "aki": "Friday",
+            "fuka": "Friday",
+            "subaru": "Thursday",
+            "masamune": "Thursday",
+            "ureshino": "Sunday",
+        }
+
+        actual = _main(self.the_seven)
+        assert actual == expected
+        assert (
+            self.the_seven
             == [name for name in actual]
             == [name for name in expected]
         )
