bbe755a3c3f4f9b688265cce880a5b2e4cdd3c95
ftnext <takuyafjp+develop@gmail.com>
2023-06-26 01:39:35 +0900

    [feat] Users can see version

--- the_solitary_castle_in_the_mirror/__main__.py
+++ the_solitary_castle_in_the_mirror/__main__.py
@@ -2,12 +2,14 @@ from __future__ import annotations
 
 import argparse
 import json
+import sys
 from collections.abc import Iterable
 from datetime import date
 
 from the_solitary_castle_in_the_mirror import (
     Character,
     Weekday,
+    __version__,
     calculate_weekday,
     characters,
 )
@@ -22,9 +24,18 @@ def _main(characters: Iterable[Character]) -> dict[Character, Weekday]:
 
 
 def main() -> None:
-    parser = argparse.ArgumentParser()
+    pre_parser = argparse.ArgumentParser(add_help=False)
+    pre_parser.add_argument(
+        "--version", "-V", action="store_true", help="show version and exit"
+    )
+    pre_args, unknown = pre_parser.parse_known_args()
+    if pre_args.version:
+        print(__version__)
+        sys.exit(0)
+
+    parser = argparse.ArgumentParser(parents=[pre_parser])
     parser.add_argument("character", nargs="+", choices=characters)
-    args = parser.parse_args()
+    args = parser.parse_args(unknown)
 
     days = _main(args.character)
 
